9b5454eab9106a159acd7ca6578013c4bd57d832
emonmorol <ekramulhasane69@gmail.com>
2022-04-27 03:51:28 +0600

    login and registration page added

--- src/Pages/Login/Registration/Registration.js
+++ src/Pages/Login/Registration/Registration.js
@@ -1,10 +1,79 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Registration = () => {
   return (
-    <div>
-      <h1>This is for registration</h1>
-    </div>
+    <>
+      <div className="lg:flex">
+        <div className="lg:w-1/2 xl:max-w-screen-sm">
+          <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+            <h2
+              className="text-center text-4xl text-indigo-900 font-display font-semibold lg:text-left xl:text-5xl
+                    xl:text-bold"
+            >
+              Registration
+            </h2>
+            <div className="mt-12">
+              <form>
+                <div>
+                  <div className="text-sm font-bold text-gray-700 tracking-wide">
+                    Email Address
+                  </div>
+                  <input
+                    className="w-full text-lg pt-2 pb-2.5 px-3  border-b  rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                    type="email"
+                    placeholder="Enter Your Email"
+                  />
+                </div>
+                <div className="mt-8">
+                  <div className="flex justify-between items-center">
+                    <div className="text-sm font-bold text-gray-700 tracking-wide">
+                      Password
+                    </div>
+                    <div>
+                      <span className="text-xs font-display font-semibold text-indigo-600 hover:text-indigo-800 cursor-pointer">
+                        Forgot Password?
+                      </span>
+                    </div>
+                  </div>
+                  <input
+                    className="w-full text-lg  pt-2 pb-2.5 px-3  border-b  rounded-lg border-gray-300 focus:outline-none focus:border-indigo-500"
+                    type="password"
+                    placeholder="Enter your password"
+                  />
+                </div>
+                <div className="mt-10">
+                  <button
+                    className="bg-indigo-500 text-gray-100 px-4 py-2.5 w-full rounded-full tracking-wide
+                                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
+                                shadow-lg"
+                  >
+                    Log In
+                  </button>
+                </div>
+              </form>
+              <div className="mt-12 text-sm font-display font-semibold text-gray-700 text-center">
+                Already Have An Account ?{" "}
+                <Link
+                  to="/login"
+                  className="cursor-pointer text-indigo-600 hover:text-indigo-800"
+                >
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:flex items-center justify-center bg-indigo-100 flex-1 h-[90vh]">
+          <div className="max-w-lg transform duration-200 hover:scale-110 cursor-pointer ">
+            <img
+              src="https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,21 +2,17 @@ import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import logo from "../../logos/ZONE.png";
+import logo from "../../../logos/ZONE.png";
 
 const Header = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-transparent text-black mb-3 z-10">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="container px-4 mx-auto flex items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link
-              className="w-1/6 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-black"
-              to="/home"
-            >
-              {/* <img src={logo} alt="" /> */}
-              Volunteer Zone
+            <Link className="w-1/12" to="/">
+              <img className="w-1/4" src={logo} alt="" />
             </Link>
             <button
               className="text-black cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -37,14 +33,15 @@ const Header = () => {
             }
             id="example-navbar-danger"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <ul className="flex flex-col items-center lg:flex-row list-none">
               <li className="nav-item">
-                <a
+                <Link
+                  to="/home"
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75"
                   href="#pablo"
                 >
-                  <span className="ml-2">Home</span>
-                </a>
+                  <span className="mt-3 ml-2">Home</span>
+                </Link>
               </li>
               <li className="nav-item">
                 <a
@@ -52,7 +49,7 @@ const Header = () => {
                   href="#pablo"
                 >
                   <i className="fab fa-twitter text-lg leading-lg text-black opacity-75"></i>
-                  <span className="ml-2">Donation</span>
+                  <span className="mt-3 ml-2">Donation</span>
                 </a>
               </li>
               <li className="nav-item">
@@ -61,7 +58,7 @@ const Header = () => {
                   href="#pablo"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-black opacity-75"></i>
-                  <span className="ml-2">Events</span>
+                  <span className="mt-3 ml-2">Events</span>
                 </a>
               </li>
               <li className="nav-item">
@@ -70,7 +67,7 @@ const Header = () => {
                   href="#pablo"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-black opacity-75"></i>
-                  <span className="ml-2">Blogs</span>
+                  <span className="mt-3 ml-2">Blogs</span>
                 </a>
               </li>
               <li className="nav-item">
@@ -80,7 +77,7 @@ const Header = () => {
                   href="#pablo"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-black opacity-75"></i>
-                  <span className="ml-2">Register</span>
+                  <span className="mt-3 ml-2">Register</span>
                 </Link>
               </li>
               <li className="nav-item">
@@ -90,7 +87,7 @@ const Header = () => {
                   href="#pablo"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-black opacity-75"></i>
-                  <span className="ml-2">Login</span>
+                  <span className="mt-3 ml-2">Login</span>
                 </Link>
               </li>
             </ul>
